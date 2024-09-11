04a6d47ddaad10dad64436658cf7163f2b4eefd0
Felipe-Batista682 <felipebatistascp@gmail.com>
2024-09-11 19:42:53 -0300

    subir api

--- src/firebaseConfig.js
+++ src/firebaseConfig.js
@@ -4,12 +4,12 @@ import { getAuth } from 'firebase/auth';
 
 // Configuração do Firebase
 const firebaseConfig = {
-  apiKey: "AIzaSyDqXwAn4MNN9fXhG4iytR-S9DE-1KXyF78",
+  apiKey: "AIzaSyDJuXbLzWCVKNUp__MydwHsjNjMucUUd7I",
   authDomain: "appauth-c5ace.firebaseapp.com",
   projectId: "appauth-c5ace",
   storageBucket: "appauth-c5ace.appspot.com",
   messagingSenderId: "527386171373",
-  appId: "1:527386171373:android:73d5698df92cccd2a239e4" // Substitua pelo seu App ID
+  appId: "1:529906816126:android:fca09e371588d1b90a742f" // Substitua pelo seu App ID
 };
 
 // Inicialize o Firebase
